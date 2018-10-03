76684f281cb91bcf6cb4b33976e75ce40a8385a8
Ed Walters <edwalters@familysearch.org>
2018-10-03 13:08:20 -0600

    small changes

--- Test/mainpage.js
+++ Test/mainpage.js
@@ -10,15 +10,6 @@
 
 var until = protractor.ExpectedConditions;
 
-
-function waitForElement(elementToFind) {
-    var dfd = protractor.promise.defer();
-    browser.wait(until.presenceOf(elementToFind), 40000, 'Element not present').then(function() {
-        dfd.fulfill();
-    });
-    return dfd.promise;
-}
-
 function waitForElementNotVisible(elementToFind) {
     var dfd = protractor.promise.defer();
     browser.wait(until.invisibilityOf(elementToFind), 40000, 'Element visible').then(function() {
@@ -41,44 +32,44 @@ function refresh() {
 var elements = {
     signIn:                     element(by.css('[class^="sign-in-link"]')),
 
-    contactCard:              element(by.css('[data-dialog-key="fs:contactCard"]')),
-    contactCardCancel:        element(by.css('[data-dialog-key="fs:contactCard"] #cancel-button')),
-    contactCardReady:         element(by.css('[data-dialog-key="fs:contactCard"][aria-busy="false"]')),
-    contactCardName:          element(by.css('[data-dialog-key="fs:contactCard"] [data-test="contactName"]')),
-    contactCardSendButton:    element(by.css('[data-dialog-key="fs:contactCard"] [data-test="sendButton"]')),
-    deleteDialogClosed:       element(by.css('[data-dialog-key="fs:confirm"][aria-hidden="true"]')),
-    deleteDialogOpened:       element(by.css('[data-dialog-key="fs:confirm"][aria-hidden="false"]')),
-    deleteDialogYesButton:    element(by.css('[data-dialog-key="fs:confirm"] [data-test="ok-button"]')),
-    deleteThreadButton:       element(by.css('[data-test="deleteThreadButton"]')),
+//    contactCard:              element(by.css('[data-dialog-key="fs:contactCard"]')),
+//    contactCardCancel:        element(by.css('[data-dialog-key="fs:contactCard"] #cancel-button')),
+//    contactCardReady:         element(by.css('[data-dialog-key="fs:contactCard"][aria-busy="false"]')),
+//    contactCardName:          element(by.css('[data-dialog-key="fs:contactCard"] [data-test="contactName"]')),
+//    contactCardSendButton:    element(by.css('[data-dialog-key="fs:contactCard"] [data-test="sendButton"]')),
+//    deleteDialogClosed:       element(by.css('[data-dialog-key="fs:confirm"][aria-hidden="true"]')),
+//    deleteDialogOpened:       element(by.css('[data-dialog-key="fs:confirm"][aria-hidden="false"]')),
+//    deleteDialogYesButton:    element(by.css('[data-dialog-key="fs:confirm"] [data-test="ok-button"]')),
+//    deleteThreadButton:       element(by.css('[data-test="deleteThreadButton"]')),
     displayName:              element(by.css('.user-menu [data-test="NavigationUserDisplayName"]')),
-    emptyMailbox:             element(by.css('[data-thread="emptyThread"]')),
-    lastMessage:              element(by.css('[data-test^="threadid:"]:last-child')),
-    lastMessageContact:       element(by.css('[data-test^="threadid:"]:last-child [data-test="messageContact"]')),
-    lastMessageBody:          element(by.css('[data-test^="threadid:"]:last-child [data-test="messageBody"]')),
-    loadMoreThreads:		  element(by.css('[data-test="loadMoreThreads"]')),
-    mainAppDiv:               element(by.css('[data-test="mailbox-app"]')),
-    messageCountRead:         element(by.css('[data-test="readCount"]')),
-    messageCountUnread:       element(by.css('[data-test="unreadCount"]')),
-    messageOne:				  element(by.css('[data-test="thread:0"]')),
-    messageTen:				  element(by.css('[data-test="thread:9"]')),
-    messageTwelve:			  element(by.css('[data-test="thread:11"]')),
-    messagesUnreadText:       element(by.css('[class="fsmb-message-counts__unread"]')),
-    page:                     element(by.css('.fsmb-message--content')),
-    pageLoaded:               element(by.css('.fsmb-message--content:not(.isLoading)')),
-    pageLoading:              element(by.css('.fsmb-message--content.isLoading')),
-    reportAbuseButton:        element(by.css('[data-test="reportAbuseButton"]')),
-    reportAbuseDialogCancel:  element(by.css('[data-dialog-key="fs:reportAbuse"] #cancel-button')),
-    reportAbuseDialogClosed:  element(by.css('[data-dialog-key="fs:reportAbuse"][aria-hidden="true"]')),
-    reportAbuseDialogOpened:  element(by.css('[data-dialog-key="fs:reportAbuse"][aria-hidden="false"]')),
-    sendButton:               element(by.css('[data-test="sendButton"]')),
-    sendMessageDialogOpened:  element(by.css('[data-dialog-key="fs:sendMessage"][aria-hidden="false"]')),
-    sendMessageDialogClose:   element(by.css('[data-dialog-key="fs:sendMessage"] [data-test="close-button"]')),
-    sendTextarea:             element(by.css('[data-test="sendTextarea"]')),
+//    emptyMailbox:             element(by.css('[data-thread="emptyThread"]')),
+//    lastMessage:              element(by.css('[data-test^="threadid:"]:last-child')),
+//    lastMessageContact:       element(by.css('[data-test^="threadid:"]:last-child [data-test="messageContact"]')),
+//    lastMessageBody:          element(by.css('[data-test^="threadid:"]:last-child [data-test="messageBody"]')),
+//    loadMoreThreads:		  element(by.css('[data-test="loadMoreThreads"]')),
+//    mainAppDiv:               element(by.css('[data-test="mailbox-app"]')),
+//    messageCountRead:         element(by.css('[data-test="readCount"]')),
+//    messageCountUnread:       element(by.css('[data-test="unreadCount"]')),
+//    messageOne:				  element(by.css('[data-test="thread:0"]')),
+//    messageTen:				  element(by.css('[data-test="thread:9"]')),
+//    messageTwelve:			  element(by.css('[data-test="thread:11"]')),
+//    messagesUnreadText:       element(by.css('[class="fsmb-message-counts__unread"]')),
+//    page:                     element(by.css('.fsmb-message--content')),
+//    pageLoaded:               element(by.css('.fsmb-message--content:not(.isLoading)')),
+//    pageLoading:              element(by.css('.fsmb-message--content.isLoading')),
+//    reportAbuseButton:        element(by.css('[data-test="reportAbuseButton"]')),
+//    reportAbuseDialogCancel:  element(by.css('[data-dialog-key="fs:reportAbuse"] #cancel-button')),
+//    reportAbuseDialogClosed:  element(by.css('[data-dialog-key="fs:reportAbuse"][aria-hidden="true"]')),
+//    reportAbuseDialogOpened:  element(by.css('[data-dialog-key="fs:reportAbuse"][aria-hidden="false"]')),
+//    sendButton:               element(by.css('[data-test="sendButton"]')),
+//    sendMessageDialogOpened:  element(by.css('[data-dialog-key="fs:sendMessage"][aria-hidden="false"]')),
+//    sendMessageDialogClose:   element(by.css('[data-dialog-key="fs:sendMessage"] [data-test="close-button"]')),
+//    sendTextarea:             element(by.css('[data-test="sendTextarea"]')),
     spinner:                  element(by.css('[class="fsmb-mailbox__spinner"]')),
-    threadAboutUrl:           element(by.css('[data-test="aboutUrl"]')),
-    threadButton:             null,
-    threadListScroll:		  '[data-test*="thread:"]',
-    threadSubject:            element(by.css('[data-test="threadSubject"][data-ng-bind-html="thread.subject"]')),
+//    threadAboutUrl:           element(by.css('[data-test="aboutUrl"]')),
+//    threadButton:             null,
+//    threadListScroll:		  '[data-test*="thread:"]',
+//    threadSubject:            element(by.css('[data-test="threadSubject"][data-ng-bind-html="thread.subject"]')),
     userButton:               element(by.css('.signedin [data-test="NavigationUserDisplayName"]'))
 };
 
--- Test/todo-spec.js
+++ Test/todo-spec.js
@@ -2,58 +2,52 @@ var mainpage = require('./mainpage.js');
 process.env.JOB_NAME = "Test1";
 var elements = mainpage.getElements();
 var until = protractor.ExpectedConditions;
-var loopTimes = 4; // number of 15 minute loops
+var loopTimes = 1; // number of 15 minute loops
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 90000;
 
 env = 'beta';  // dev, beta, prod
 
-describe("### Current SessionID\n", function() {
+describe("### Current SessionID\n", function () {
 
-    beforeEach(function(done) {
+    beforeEach(function (done) {
         browser.ignoreSynchronization = true;
         done();
     });
 
     it("should set env", function () {
-       if(env === 'dev'){
-           env = 'https://integration.familysearch.org';
-       }
-       else if(env === 'beta') {
-           env = 'https://beta.familysearch.org';
-       }
-       else {
-           env = 'https://familysearch.org';
-       }
+        if (env === 'dev') {
+            env = 'https://integration.familysearch.org';
+        }
+        else if (env === 'beta') {
+            env = 'https://beta.familysearch.org';
+        }
+        else {
+            env = 'https://familysearch.org';
+        }
     });
 
-    it("should open web page", function(done) {
+    it("should open web page", function (done) {
 //        var displayNameText;
         browser.get(env);
         //browser.pause();
-        browser.wait(until.presenceOf(elements.signIn), 40000, 'SignIn not there').then(function() {
-            elements.signIn.click().then(function() {
+        browser.wait(until.presenceOf(elements.signIn), 40000, 'SignIn not there').then(function () {
+            elements.signIn.click().then(function () {
                 browser.sleep(18000);//.then(function () {
-//                     browser.getTitle().then(function(title) {
-//                         elements.displayName.getText().then(function (displayNameText) {
-//                             console.log(displayNameText);
-//                         });
-//                         expect(elements.displayName.getText()).toMatch('Edward A Walters');
-//                         console.log(title);
-//                     });
-//                 });
-                console.log("\n" + env);
+                console.log("\n Environment: ", env);
             });
         });
         done();
     });
 
-    it("should keep session alive", function() {
+    it("should keep session alive", function () {
 //        jasmine.DEFAULT_TIMEOUT_INTERVAL = 3600000; //3600000 = one hour
-        mainpage.getSessionId().then(function () {
-//            console.log(session);
+        mainpage.getSessionId().then(function (session) {
+            console.log(session);
             mainpage.keepAliveLoop(loopTimes);
         });
 
     }, 7200000);
 
 });
+
+// Turn in to async/await
