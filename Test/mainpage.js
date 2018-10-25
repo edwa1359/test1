80ea43c6e8815bcbfb14909fe6c4c0d0e91f0b5e
Ed Walters <edwalters@familysearch.org>
2018-10-25 06:48:07 -0600

    put sessionId into the log

--- Test/mainpage.js
+++ Test/mainpage.js
@@ -1,142 +1,197 @@
 /*
  * Created by Ed Walters 11/2017
  */
-//var env = browser.testEnv;
 
-// function sendKeys(element, keys) {
-//     utils.waitForElement(element);
-//     element.sendKeys(keys);
-// };
+let until = protractor.ExpectedConditions;
 
-var until = protractor.ExpectedConditions;
-
-function waitForElementNotVisible(elementToFind) {
-    var dfd = protractor.promise.defer();
-    browser.wait(until.invisibilityOf(elementToFind), 40000, 'Element visible').then(function() {
-        dfd.fulfill();
-    });
-    return dfd.promise;
+async function waitForElementNotVisible(elementToFind) {
+    browser.wait(until.invisibilityOf(elementToFind), 40000, 'Element visible');
 }
 
-function refresh() {
-    var dfd = protractor.promise.defer();
-    browser.refresh();
-    waitForElementNotVisible(elements.spinner).then(function () {
-//        expect(elements.displayName.getText()).toMatch('Edward A Walters');
-        dfd.fulfill();
-//            browser.sleep(100);
-    });
-    return dfd.promise;
+async function refresh() {
+    await browser.refresh();
+    waitForElementNotVisible(elements.spinner);
 }
 
-var elements = {
-    signIn:                     element(by.css('[class^="sign-in-link"]')),
-
-//    contactCard:              element(by.css('[data-dialog-key="fs:contactCard"]')),
-//    contactCardCancel:        element(by.css('[data-dialog-key="fs:contactCard"] #cancel-button')),
-//    contactCardReady:         element(by.css('[data-dialog-key="fs:contactCard"][aria-busy="false"]')),
-//    contactCardName:          element(by.css('[data-dialog-key="fs:contactCard"] [data-test="contactName"]')),
-//    contactCardSendButton:    element(by.css('[data-dialog-key="fs:contactCard"] [data-test="sendButton"]')),
-//    deleteDialogClosed:       element(by.css('[data-dialog-key="fs:confirm"][aria-hidden="true"]')),
-//    deleteDialogOpened:       element(by.css('[data-dialog-key="fs:confirm"][aria-hidden="false"]')),
-//    deleteDialogYesButton:    element(by.css('[data-dialog-key="fs:confirm"] [data-test="ok-button"]')),
-//    deleteThreadButton:       element(by.css('[data-test="deleteThreadButton"]')),
+let elements = {
+    signIn:                   element(by.css('[class^="sign-in-link"]')),
     displayName:              element(by.css('.user-menu [data-test="NavigationUserDisplayName"]')),
-//    emptyMailbox:             element(by.css('[data-thread="emptyThread"]')),
-//    lastMessage:              element(by.css('[data-test^="threadid:"]:last-child')),
-//    lastMessageContact:       element(by.css('[data-test^="threadid:"]:last-child [data-test="messageContact"]')),
-//    lastMessageBody:          element(by.css('[data-test^="threadid:"]:last-child [data-test="messageBody"]')),
-//    loadMoreThreads:		  element(by.css('[data-test="loadMoreThreads"]')),
-//    mainAppDiv:               element(by.css('[data-test="mailbox-app"]')),
-//    messageCountRead:         element(by.css('[data-test="readCount"]')),
-//    messageCountUnread:       element(by.css('[data-test="unreadCount"]')),
-//    messageOne:				  element(by.css('[data-test="thread:0"]')),
-//    messageTen:				  element(by.css('[data-test="thread:9"]')),
-//    messageTwelve:			  element(by.css('[data-test="thread:11"]')),
-//    messagesUnreadText:       element(by.css('[class="fsmb-message-counts__unread"]')),
-//    page:                     element(by.css('.fsmb-message--content')),
-//    pageLoaded:               element(by.css('.fsmb-message--content:not(.isLoading)')),
-//    pageLoading:              element(by.css('.fsmb-message--content.isLoading')),
-//    reportAbuseButton:        element(by.css('[data-test="reportAbuseButton"]')),
-//    reportAbuseDialogCancel:  element(by.css('[data-dialog-key="fs:reportAbuse"] #cancel-button')),
-//    reportAbuseDialogClosed:  element(by.css('[data-dialog-key="fs:reportAbuse"][aria-hidden="true"]')),
-//    reportAbuseDialogOpened:  element(by.css('[data-dialog-key="fs:reportAbuse"][aria-hidden="false"]')),
-//    sendButton:               element(by.css('[data-test="sendButton"]')),
-//    sendMessageDialogOpened:  element(by.css('[data-dialog-key="fs:sendMessage"][aria-hidden="false"]')),
-//    sendMessageDialogClose:   element(by.css('[data-dialog-key="fs:sendMessage"] [data-test="close-button"]')),
-//    sendTextarea:             element(by.css('[data-test="sendTextarea"]')),
     spinner:                  element(by.css('[class="fsmb-mailbox__spinner"]')),
-//    threadAboutUrl:           element(by.css('[data-test="aboutUrl"]')),
-//    threadButton:             null,
-//    threadListScroll:		  '[data-test*="thread:"]',
-//    threadSubject:            element(by.css('[data-test="threadSubject"][data-ng-bind-html="thread.subject"]')),
     userButton:               element(by.css('.signedin [data-test="NavigationUserDisplayName"]'))
-};
+}
 
 module.exports =  {
-    getElements: function() {
+    getElements:  function() {
         return elements;
     },
 
-    // refresh: function() {
-    //     var dfd = protractor.promise.defer();
-    //     refresh().then(function () {
-    //         dfd.fulfill();
-    //     });
-    //     return dfd.promise;
-    // },
-
-    keepAliveLoop: function (amt) {
-        var dfd = protractor.promise.defer();
-        function doLoop(idx) {
-            if(idx < amt){ // set this to the number of 15 minute loops to stay alive
-                browser.sleep(900000); // 15 minutes
+    keepAliveLoop: async function (amt) {
+        async function doLoop(idx) {
+            if(idx < amt){
+                await browser.sleep(30000); // 15 minutes = 900000
                 console.log("loop times: " + (idx + 1));
-                refresh().then(function () {
-                    doLoop(idx + 1);
-                });
-            }
-            else {
-                dfd.fulfill();
+                await refresh();
+                await doLoop(idx + 1);
             }
         }
-        doLoop(0);
-        return dfd.promise;
+        await doLoop(0);
     },
 
-    getSessionId:function () {
-        var dfd = protractor.promise.defer();
-        browser.executeScript(function () {
-            function readCookie(name) {
-                var nameEQ = name + "=";
-                var ca = document.cookie.split(';');
-                for (var i = 0; i < ca.length; i++) {
-                    var c = ca[i];
-                    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-                    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-                }
-                return null;
-            }
-            function copyToClipboard(text) {
-                document.body.innerText = text;
-                var range = document.createRange();
-                range.selectNode(document.body);
-                window.getSelection().addRange(range);
-                try {
-                    var successful = document.execCommand('copy');
-                    var msg = successful ? 'successful' : 'unsuccessful';
-                     console.log("Copy email command was " + msg);
-                     console.log("SessionId: " + range);
-                }
-                catch(err) {
-                    console.log('Oops, unable to copy');
-                }
-                window.getSelection().removeAllRanges();
-            }
-            copyToClipboard(readCookie('fssessionid'));
-        });
-        dfd.fulfill();
-        return dfd.promise;
+    getSessionId: async function () {
+        let session, cookie;
+        cookie = await browser.manage().getCookie('familysearch-sessionid');
+//        console.log("cookie: ", cookie);
+        session = cookie.value;
+//        console.log("sessionID: " + session);
+//        console.log(session);
+        return session;
+    },
+
+    getTime: async function(text) {
+        let d = new Date();
+        let h = d.getHours();
+        if(h < 10){
+            h = ("0" + h).slice(-2);
+        }
+        let m = d.getMinutes();
+        if(m < 10){
+            m = ("0" + m).slice(-2);
+        }
+        let s = d.getSeconds();
+        if(s < 10) {
+            s= ("0" + s).slice(-2);
+        }
+        let ms = d.getMilliseconds();
+        await console.log(text + ": " + h + ":" + m + ":" + s + ":" + ms);
+    },
+
+    waitForElementClickable: async function(elementToFind) {
+        return await browser.wait(until.elementToBeClickable(elementToFind), 40000,"Waited 40 seconds for element to be clickable");
     }
 
+
+//     getSessionId: async function () {
+//         let session;
+//         await browser.executeScript(async function () {
+//             async function readCookie(name) {
+//                 let nameEQ = name + "=";
+//                 let ca = document.cookie.split(';');
+//                 for (let i = 0; i < ca.length; i++) {
+//                     let c = ca[i];
+//                     while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+//                     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+//                 }
+// //                return null;
+//             }
+//             async function copyToClipboard(text) {
+//                 document.body.innerText = text;
+//                 let range = document.createRange();
+//                 await range.selectNode(document.body);
+//                 await window.getSelection().addRange(range);
+//                 try {
+//                     let successful = document.execCommand('copy');
+//                     let msg = successful ? 'successful' : 'unsuccessful';
+//                     console.log("Copy email command was " + msg);
+//                     console.log("SessionId: " + range);
+//                     session = range;
+//                 }
+//                 catch(err) {
+//                     console.log('Oops, unable to copy');
+//                 }
+//                 await window.getSelection().removeAllRanges();
+//             }
+//             await copyToClipboard(readCookie('fssessionid'));
+//         });
+//         return session;
+//     }
+
 };
+
+
+// var until = protractor.ExpectedConditions;
+//
+// function waitForElementNotVisible(elementToFind) {
+//     var dfd = protractor.promise.defer();
+//     browser.wait(until.invisibilityOf(elementToFind), 40000, 'Element visible').then(function() {
+//         dfd.fulfill();
+//     });
+//     return dfd.promise;
+// }
+//
+// function refresh() {
+//     var dfd = protractor.promise.defer();
+//     browser.refresh();
+//     waitForElementNotVisible(elements.spinner).then(function () {
+// //        expect(elements.displayName.getText()).toMatch('Edward A Walters');
+//         dfd.fulfill();
+// //            browser.sleep(100);
+//     });
+//     return dfd.promise;
+// }
+//
+// var elements = {
+//     signIn:                     element(by.css('[class^="sign-in-link"]')),
+//     displayName:              element(by.css('.user-menu [data-test="NavigationUserDisplayName"]')),
+//     spinner:                  element(by.css('[class="fsmb-mailbox__spinner"]')),
+//     userButton:               element(by.css('.signedin [data-test="NavigationUserDisplayName"]'))
+// };
+//
+// module.exports =  {
+//     getElements: function() {
+//         return elements;
+//     },
+//
+//     keepAliveLoop: function (amt) {
+//         var dfd = protractor.promise.defer();
+//         function doLoop(idx) {
+//             if(idx < amt){ // set this to the number of 15 minute loops to stay alive
+//                 browser.sleep(900000); // 15 minutes
+//                 console.log("loop times: " + (idx + 1));
+//                 refresh().then(function () {
+//                     doLoop(idx + 1);
+//                 });
+//             }
+//             else {
+//                 dfd.fulfill();
+//             }
+//         }
+//         doLoop(0);
+//         return dfd.promise;
+//     },
+//
+//     getSessionId:function () {
+//         var dfd = protractor.promise.defer();
+//         browser.executeScript(function () {
+//             function readCookie(name) {
+//                 var nameEQ = name + "=";
+//                 var ca = document.cookie.split(';');
+//                 for (var i = 0; i < ca.length; i++) {
+//                     var c = ca[i];
+//                     while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+//                     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+//                 }
+//                 return null;
+//             }
+//             function copyToClipboard(text) {
+//                 document.body.innerText = text;
+//                 var range = document.createRange();
+//                 range.selectNode(document.body);
+//                 window.getSelection().addRange(range);
+//                 try {
+//                     var successful = document.execCommand('copy');
+//                     var msg = successful ? 'successful' : 'unsuccessful';
+//                      console.log("Copy email command was " + msg);
+//                      console.log("SessionId: " + range);
+//                 }
+//                 catch(err) {
+//                     console.log('Oops, unable to copy');
+//                 }
+//                 window.getSelection().removeAllRanges();
+//             }
+//             copyToClipboard(readCookie('fssessionid'));
+//         });
+//         dfd.fulfill();
+//         return dfd.promise;
+//     }
+//
+// };
--- Test/todo-spec.js
+++ Test/todo-spec.js
@@ -1,11 +1,11 @@
-var mainpage = require('./mainpage.js');
+let mainpage = require('./mainpage.js');
 process.env.JOB_NAME = "Test1";
-var elements = mainpage.getElements();
-var until = protractor.ExpectedConditions;
-var loopTimes = 1; // number of 15 minute loops
+let elements = mainpage.getElements();
+//let until = protractor.ExpectedConditions;
+let loopTimes = 2; // number of 15 minute loops
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 90000;
 
-env = 'beta';  // dev, beta, prod
+let env = 'dev';  // dev, beta, prod
 
 describe("### Current SessionID\n", function () {
 
@@ -14,7 +14,7 @@ describe("### Current SessionID\n", function () {
         done();
     });
 
-    it("should set env", function () {
+    it("should set env", async function () {
         if (env === 'dev') {
             env = 'https://integration.familysearch.org';
         }
@@ -26,27 +26,69 @@ describe("### Current SessionID\n", function () {
         }
     });
 
-    it("should open web page", function (done) {
-//        var displayNameText;
-        browser.get(env);
-        //browser.pause();
-        browser.wait(until.presenceOf(elements.signIn), 40000, 'SignIn not there').then(function () {
-            elements.signIn.click().then(function () {
-                browser.sleep(18000);//.then(function () {
-                console.log("\n Environment: ", env);
-            });
-        });
-        done();
+//     it("should set env", function () {
+//         if (env === 'dev') {
+//             env = 'https://integration.familysearch.org';
+//         }
+//         else if (env === 'beta') {
+//             env = 'https://beta.familysearch.org';
+//         }
+//         else {
+//             env = 'https://familysearch.org';
+//         }
+//     });
+//
+
+    it("should open web page", async function () {
+        await browser.get(env);
+//        await browser.wait(await until.presenceOf(elements.signIn), 40000, 'SignIn not there');
+        await mainpage.waitForElementClickable(elements.signIn);
+//        await browser.sleep(5000);
+        await elements.signIn.click();
+        await browser.sleep(11000);
+        await console.log("\n Environment: ", env);
     });
 
-    it("should keep session alive", function () {
-//        jasmine.DEFAULT_TIMEOUT_INTERVAL = 3600000; //3600000 = one hour
-        mainpage.getSessionId().then(function (session) {
-            console.log(session);
-            mainpage.keepAliveLoop(loopTimes);
-        });
 
+
+//     it("should open web page", function (done) {
+// //        var displayNameText;
+//         browser.get(env);
+//         //browser.pause();
+//         browser.wait(until.presenceOf(elements.signIn), 40000, 'SignIn not there').then(function () {
+//             elements.signIn.click().then(function () {
+//                 browser.sleep(18000);//.then(function () {
+//                 console.log("\n Environment: ", env);
+//             });
+//         });
+//        done();
+//     });
+
+    it("should keep session alive", async function () {
+        let session;
+        session = await mainpage.getSessionId();
+        await console.log("\n");
+        await console.log(session);
+        await console.log("\n");
+        await mainpage.getTime("Start");
+        await mainpage.keepAliveLoop(loopTimes);
+        await mainpage.getTime("End");
+        await console.log("\n");
     }, 7200000);
+
+    // it("should get finish time", async function () {
+    //    await mainpage.getTime("End");
+    // });
+
+//
+//     it("should keep session alive", function () {
+// //        jasmine.DEFAULT_TIMEOUT_INTERVAL = 3600000; //3600000 = one hour
+//         mainpage.getSessionId().then(function (session) {
+//             console.log(session);
+//             mainpage.keepAliveLoop(loopTimes);
+//         });
+//
+//     }, 7200000);
 
 });
 
